393485ea554e55f42c54fcb3037a8d6660cc4277
shrikant2753 <shrikantrp.00@gmail.com>
2023-04-02 11:06:06 +0530

    solution

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -1,12 +1,14 @@
 package com.driver.services.impl;
 
-import com.driver.model.User;
+import com.driver.model.*;
 import com.driver.repository.ConnectionRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
 import com.driver.services.ConnectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ConnectionServiceImpl implements ConnectionService {
@@ -32,7 +34,42 @@ public class ConnectionServiceImpl implements ConnectionService {
                 throw new Exception("Unable to connect");
             }
 
+            List<ServiceProvider> serviceProviderList = user.getServiceProviderList();
+            int a = Integer.MAX_VALUE;
+            ServiceProvider serviceProvider=null;
+            Country country = null;
 
+            for(ServiceProvider serviceProvider1 : serviceProviderList){
+                List<Country> countryList = serviceProvider1.getCountryList();
+
+                for(Country country1 : countryList){
+                    if(countryName.equalsIgnoreCase(country1.getCountryName().toString()) &&
+                            a > serviceProvider1.getId()){
+                        a = serviceProvider1.getId();
+                        serviceProvider = serviceProvider1;
+                        country = country1;
+                    }
+                }
+            }
+
+            if (serviceProvider!=null){
+                Connection connection = new Connection();
+                connection.setUser(user);
+                connection.setServiceProvider(serviceProvider);
+
+                String cc = country.getCode();
+                int givenId = serviceProvider.getId();
+                String mask = cc+"."+givenId+"."+userId;
+
+                user.setMaskedIp(mask);
+                user.setConnected(true);
+                user.getConnectionList().add(connection);
+
+                serviceProvider.getConnectionList().add(connection);
+
+                userRepository2.save(user);
+                serviceProviderRepository2.save(serviceProvider);
+            }
 
         }
         return  user;
